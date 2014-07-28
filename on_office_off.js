f1a544b0ca51e15336c25a97134d525a46f80b75
BlackLight <blacklight@autistici.org>
2014-07-28 19:28:14 +0200

    Modified on_home_off and added on_office_off

--- on_office_off.js
+++ on_office_off.js
@@ -1,11 +1,10 @@
-setGlobal("HOME", "0");
+setGlobal("OFFICE", "0");
 
 if (global("WIFICONNECTED") == 0) {
 	mobileData(true);
 	setAutoSync(false);
 	setWifi(false);
 	setGlobal("HOME", "0");
-	setGlobal("OFFICE", "0");
 
 	flash("Entering outdoor mode");
 }
